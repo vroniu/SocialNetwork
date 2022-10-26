7c0bdeb13bfcca2aa08a8e438e1364d5e051b615
vroniu <gwrona1@gmail.com>
2022-10-26 23:35:35 +0200

    Initial commit

---
+++ src/test/java/com/example/SocialNetwork/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SocialNetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
